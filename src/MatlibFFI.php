93f18d6fba30a8095f7c59552d10b093d18a65b6
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-04-28 23:12:04 +0900

    logging

--- src/MatlibFFI.php
+++ src/MatlibFFI.php
@@ -34,27 +34,35 @@ class MatlibFFI extends AbstractMatlibService
         }
         if($openblasFactory===null && class_exists(OpenBLASFactory::class)) {
             $openblasFactory = new OpenBLASFactory();
-            $this->logging(0,get_class($openblasFactory).' found.');
+            $this->logging(0,get_class($openblasFactory).'** not found ** .');
+        } elseif($mathFactory===null) {
+            $this->logging(0,OpenBLASFactory::class.'** not found **.');
         }
         if($mathFactory===null && class_exists(MatlibFactory::class)) {
             $mathFactory = new MatlibFactory();
             $this->logging(0,get_class($mathFactory).' found.');
         } elseif($mathFactory===null) {
-            $this->logging(0,MatlibFactory::class.'not found.');
+            $this->logging(0,MatlibFactory::class.'** not found **.');
         }
         if($openclFactory===null && class_exists(OpenCLFactory::class)) {
             $openclFactory = new OpenCLFactory();
             $this->logging(0,get_class($openclFactory).' found.');
+        } elseif($mathFactory===null) {
+            $this->logging(0,OpenCLFactory::class.'** not found **.');
         }
         $bufferCLFactory = $bufferCLFactory ?? $openclFactory;
         if($clblastFactory===null && class_exists(CLBlastFactory::class)) {
             $clblastFactory = new CLBlastFactory();
             $this->logging(0,get_class($clblastFactory).' found.');
+        } elseif($mathFactory===null) {
+            $this->logging(0,CLBlastFactory::class.'** not found **.');
         }
         $blasCLFactory = $blasCLFactory ?? $clblastFactory;
         if($mathCLFactory===null && class_exists(MatlibCLFactory::class)) {
             $mathCLFactory = new MatlibCLFactory();
             $this->logging(0,get_class($mathCLFactory).' found.');
+        } elseif($mathFactory===null) {
+            $this->logging(0,MatlibCLFactory::class.'** not found **.');
         }
 
         parent::__construct(
